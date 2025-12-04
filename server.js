1c5a6036497624c70dbb10558eb28ede0f805223
Kavya Suma <kavyagali914@gmail.com>
2025-12-04 23:03:42 +0530

    Prepare for deployment on Render

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.get("/", (req, res) => {
   res.send("Microblogging API is running!");
 });
 
-// Start server
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
