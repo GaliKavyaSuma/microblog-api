8fed909b0a86c1296755882ada543628b2b8d7bc
Kavya Suma <kavyagali914@gmail.com>
2025-12-04 23:52:55 +0530

    Initial commit - Microblog API

--- server.js
+++ server.js
@@ -18,9 +18,13 @@ app.get("/", (req, res) => {
   res.send("Microblogging API is running!");
 });
 
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+//app.listen(PORT, () => {
+//  console.log(`Server running on port ${PORT}`);
+//});
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
-
